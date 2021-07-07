e6e3d074e44f46f46208a999f3061ae03ba06aa1
Aziz-Rashid <axixraxhid@gmail.com>
2021-07-08 03:27:08 +0500

    update

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState,useContext } from "react";
 import Head from "next/head";
 import Header from "../components/Home/Header";
 import BannerSection from "../components/Home/BannerSection";
@@ -10,10 +10,15 @@ import HowDoesItWork from "../components/Home/HowDoesItWork";
 import RoadMapSection from "../components/Home/RoadMapSection";
 import IndustrySection from "../components/Home/IndustrySection";
 import Footer from "../components/Home/Footer";
+import { Web3Context } from "../context/Web3ContexT";
+import { AssetsContext } from "../context/AssetsContext";
 
 const Home = () => {
   const [screenWidth, setScreenWidth] = useState(0);
+  const [assetsState, setAssetsState] = useContext(AssetsContext);
+  const [web3State, setWeb3State] = useContext(Web3Context);
 
+  console.log(web3State)
   useEffect(() => {
     const handleResize = () => {
       setScreenWidth(window.innerWidth);
